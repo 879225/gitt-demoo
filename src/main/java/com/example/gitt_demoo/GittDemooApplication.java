066c342fc5967a21ee470249716a599332645f54
varsha <varshapoojar643@gmail.com>
2025-04-16 21:03:19 +0530

    first commit - project initiated

---
+++ src/main/java/com/example/gitt_demoo/GittDemooApplication.java
@@ -0,0 +1,13 @@
+package com.example.gitt_demoo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GittDemooApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GittDemooApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/gitt_demoo/GittDemooApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.gitt_demoo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GittDemooApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
